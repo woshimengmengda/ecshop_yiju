59e8cbcce3e0d127325be37f7a374b727d9a4463
woshimengmengda <kekezaixian@qq.com>
2017-10-22 03:30:53 +0800

    会员分页同步接口

--- shop/interface/request/user.php
+++ shop/interface/request/user.php
@@ -27,63 +27,69 @@ if($action == 'page_sync_user'){#会员同步接口 定时任务去跑
 //    $user = get_user_by_cardnumber('46694100211');
 //    echo "<pre>";
 //    print_r($user);exit;
-    $param = array(
-        'modifyDateTime' => date("Y-m-d H:i:s", time()-180),
-        'page' => 1
-    );
-    $res = page_sync_user($param);
-//    $result = json_decode($res, true);
-    //同步成功返回数据
-    if($result['code'] == '0'){
-        foreach ((array)$result['DateDetail'] as $mk => $mv){
-            switch ($mv['type']){
-                //离职员工 删除
-                case '3':
-                    $user = get_user_by_cardnumber($mv['cardNumber']);
-                    //存在离职员工则删除 根据兜礼文档说明进行此操作
-                    if(!empty($user)){
-                        delete_user($user['user_id']);
-                    }
-                    break;
-                //0：员工1：家属2：退休
-                case '0':
-                case '1':
-                case '2':
-                    $user = get_user_by_cardnumber($mv['cardNumber']);
-                    //存在但是手机号不相同则更新手机号
-                    if(!empty($user)){
-                        if($user['user_name'] != $mv['telephone']){
-                            $params['user_id'] = $user['user_id'];
-                            $params['mobile'] = $mv['telephone'];
-                            if(update_user_name($params)){
-                                echo "存在相同卡号会员，手机号不同，更新手机号成功";
-                            }else{
-                                echo "存在相同卡号会员，手机号不同，更新手机号失败";
-                            }
-                        }
-                    }else{//不存在则添加
-                        $username = $mv['telephone'];
-                        $password = $mv['cardNumber'];
-                        $email = '';
-                        $res = $GLOBALS['user']->add_user($username, $password, $email);
-                        //更新新增会员卡号
-                        if($res){
-                            $params['cardnumber'] = $mv['cardNumber'];
-                            $params['user_name'] = $mv['telephone'];
-                            if(update_user_cardnumber($params)){
-                                echo "新增会员成功，更新会员卡号成功";
-                            }else{
-                                echo "新增会员成功，更新会员卡号失败";
-                            }
-                        }else{
-                            echo "新增会员失败";
-                        }
+    $param['modifyDateTime'] = date("Y-m-d H:i:s", time()-180);
+    $page = 1;
+//    $TotalPage = 5;
+    do{
+        $param['page'] = $page;
+        $res = page_sync_user($param);
 
-                    }
-                    break;
+//    $result = json_decode($res, true);
+        //同步成功返回数据
+        if($res['code'] == '0'){
+            foreach ((array)$result['DateDetail'] as $mk => $mv){
+                switch ($mv['type']){
+                    //离职员工 删除
+                    case '3':
+                        $user = get_user_by_cardnumber($mv['cardNumber']);
+                        //存在离职员工则删除 根据兜礼文档说明进行此操作
+                        if(!empty($user)){
+                            delete_user($user['user_id']);
+                        }
+                        break;
+                    //0：员工1：家属2：退休
+                    case '0':
+                    case '1':
+                    case '2':
+                        $user = get_user_by_cardnumber($mv['cardNumber']);
+                        //存在但是手机号不相同则更新手机号
+                        if(!empty($user)){
+                            if($user['user_name'] != $mv['telephone']){
+                                $params['user_id'] = $user['user_id'];
+                                $params['mobile'] = $mv['telephone'];
+                                if(update_user_name($params)){
+                                    echo "存在相同卡号会员，手机号不同，更新手机号成功";
+                                }else{
+                                    echo "存在相同卡号会员，手机号不同，更新手机号失败";
+                                }
+                            }
+                        }else{//不存在则添加
+                            $username = $mv['telephone'];
+                            $password = $mv['cardNumber'];
+                            $email = '';
+                            $res = $GLOBALS['user']->add_user($username, $password, $email);
+                            //更新新增会员卡号
+                            if($res){
+                                $params['cardnumber'] = $mv['cardNumber'];
+                                $params['user_name'] = $mv['telephone'];
+                                if(update_user_cardnumber($params)){
+                                    echo "新增会员成功，更新会员卡号成功";
+                                }else{
+                                    echo "新增会员成功，更新会员卡号失败";
+                                }
+                            }else{
+                                echo "新增会员失败";
+                            }
+
+                        }
+                        break;
+                }
             }
+
         }
-    }
+        $page++;
+    }while($page<=$res['TotalPage']);
+
 //    error_log("\n 兜礼会员同步接口params \n".var_export($param, 1)."\n 结果res \n".var_export($res, 1)."\n", 3, ROOT_PATH."elog.log");
 }elseif($action == 'dooly_login'){
     //兜礼一号通登录接口 by xiaoq 2017-10-22
